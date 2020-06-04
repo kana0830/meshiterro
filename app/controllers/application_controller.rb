08da26fa097267c9d2995d4857115b7f7151a04d
vagrant <kf0830s@gmail.com>
2020-06-04 12:58:14 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  # ＝＝＝＝＝＝＝＝ここからdevise機能＝＝＝＝＝＝＝＝
+  # nameでのデータ操作がsign_upの前に実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # ログイン認証されていなければ、ログイン画面へリダイレクトする
+  before_action :authenticate_user!
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # ＝＝＝＝＝＝＝＝ここまでdevise機能＝＝＝＝＝＝＝＝
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5a77c0a3e6e1c765813616443ae88e04d6a4ca44cffa6df86ca584499341899354f6b0e92ed6ab1d1083383c99bce206b99443951025506139b0df9dc2128b5b'
